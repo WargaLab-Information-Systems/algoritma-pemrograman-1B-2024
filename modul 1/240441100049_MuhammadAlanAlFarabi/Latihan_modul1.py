cd1fd7ba4eb5a14af38ea1a9b4b66c632d57bbc5
Alssfraby <alanfarabi4@gmail.com>
2024-09-25 14:10:47 +0700

    240441100049_MuhammadAlanAlFarabi_Modul1_kakYahya

---
+++ modul 1/240441100049_MuhammadAlanAlFarabi/Latihan_modul1.py
@@ -0,0 +1,48 @@
+nama = "alan"
+umur = 18
+tingi_badan = 162
+
+print  ("nama saya adalah " +nama ," Umur saya " +umur, " tinggi badan saya " +tingi_badan)
+
+
+#operator aritmatika
+a=12
+b=5
+
+penjumlahan=a+b
+pengurangan=a-b
+perkalian=a*b
+pembagian=a/b
+modulus=a%b
+pangkat=a**b
+bilangan_bulat=a//b
+
+print(penjumlahan)
+print(pengurangan)
+print(perkalian)
+print(pembagian)
+print(modulus)
+print(pangkat)
+print(bilangan_bulat)
+
+#operator Assignment
+
+
+#operator Himpunan
+a={1,2,3}
+b={3,4,5}
+
+penggabungan= a|b
+irisan=a&b
+selisih=a-b
+selisih_simetris=a^b
+
+print(penggabungan)
+print(irisan)
+print(selisih)
+print(selisih_simetris)
+
+#operator ternery
+umur=18
+status="dewasa" if umur>=18 else "belum dewasa"
+print(status)
