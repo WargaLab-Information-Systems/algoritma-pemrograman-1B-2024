b3c6a6440d41a77953c2245d56c3b3d973cae613
andhikkadd <mandhkbprtma.316@gmail.com>
2024-09-28 21:01:08 +0700

    240441100141_M.AndhikaBagusP_Modul1_AsprakKakIqbal

--- modul 1/240441100141_M.AndhikaBagusP/tugas1.py
+++ modul 1/240441100141_M.AndhikaBagusP/tugas1.py
@@ -1,1 +1,18 @@
-print("Hello")
+# input data
+panjangBalok = 20 
+lebarBalok = 13
+tinggiBalok = 7
+diameterTabung = 14
+jarijariTabung = int(diameterTabung / 2)
+luasSelimut = 440 
+ 
+# mencari volume celengan berbentuk balok
+volumeBalok = int(panjangBalok * lebarBalok * tinggiBalok)
+
+# mencari tinggi dari celengan berbentuk tabung
+tinggiTabung = int(luasSelimut / (2 * 22/7 * jarijariTabung))
+# mencari volume celengan berbentuk tabung
+volumeTabung = int(22/7 * (jarijariTabung ** 2) * tinggiTabung)
+
+# Hasil
+print(f" Volume celengan berbentuk balok adalah {volumeBalok} cm³ dan volume celengan berbentuk tabung adalah {volumeTabung} cm³")
