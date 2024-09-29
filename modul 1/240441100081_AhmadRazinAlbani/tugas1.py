22c195139a7b669d7c5a6cf44b5b73ad12091670
razin26 <razinahmad24@gmail.com>
2024-09-29 21:24:38 +0700

    240441100081_AhmadRazinAlbani_Modul1_AsprakKakAngga

--- modul 1/240441100081_AhmadRazinAlbani/tugas1.py
+++ modul 1/240441100081_AhmadRazinAlbani/tugas1.py
@@ -1,1 +1,28 @@
-print("hello world")
+# print("hello world")
+
+# praktikum day1 
+
+nama = "razin"
+umur = 18 
+tinggibadan = 170.9
+print(" Nama saya" , nama , "umur saya" , umur , ",tinggi badan" , tinggibadan)
+
+#operator aritmatika
+a = 12
+b = 5 
+
+penjumlahan = a + b
+pengurangan = a - b
+perkalian = a*b
+pembagian = a / b
+pangkat = a ** b
+pembagianbulatkebawah = a//b
+sisahasilbagi = a % b 
+
+#operatorhimpunan 
+
+#opertaorternary 
+umur = 18
+status = "Dewasa" if umur >= 18 else "Belum Dewasa"
+print(status)
+
