f37c5079b19b20aef53fc54c4604e81ed4daf951
fauzulakbar12345678 <fauzulakbar456>
2024-09-30 21:46:11 +0700

    240441100149_AchFauzulAkbar_Modul1_AsprakKakIqbal

--- modul 1/240441100149_Ach. Fauzul Akbar/volume.py
+++
@@ -1,8 +0,0 @@
-# volume tabung 1
-import math
-diameter_tabung = 14 
-luas_selimut = 440
-jari_jari_tabung = diameter_tabung / 2
-tinggi_tabung = luas_selimut / (2 * math.pi * jari_jari_tabung)
-volume_tabung = math.pi * jari_jari_tabung**2 * tinggi_tabung
-print(f"hasil,{volume_tabung}, cm³")
