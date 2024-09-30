c2a2bc28815620d3dfa45cc80b166e44126f538c
NiaRamadani23 <niararamadani871@gmail.com>
2024-09-30 15:06:51 +0700

    240441100085_NiaRamadani_Modul1_AsprakKakAngga

---
+++ modul 1/240441100085_NiaRamadani_TugasPraktikum_Modul1_AsprakKakAngga/SOAL 1.py
@@ -0,0 +1,30 @@
+# SOAL 1
+# DATA CELENGAN BALOK
+Panjang =20  #cm
+Lebar = 13   #cm
+Tinggi = 7   #cm
+
+print("volume celengan balok milik Andi adalah")
+Volume = Panjang * Lebar * Tinggi
+print(Volume)
+
+
+# DATA CELENGAN TABUNG
+Diameter = 14       #cm
+Luas_Selimut = 440  #cm
+
+print("volume celengan tabung milik Andi adalah")
+r =int(Diameter / 2)
+phi = 22/7
+Tinggi_tabung = int(Luas_Selimut/(2*phi*r))
+Volume = int(phi * r * r * Tinggi_tabung)
+print(Volume)
+
+
+
+
+
+
+
+
+
