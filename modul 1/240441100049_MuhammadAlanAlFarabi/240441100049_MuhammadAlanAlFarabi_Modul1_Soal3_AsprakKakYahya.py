94965135359a4f341c09d478b792a57eeb85239c
Alssfraby <alanfarabi4@gmail.com>
2024-10-01 13:15:31 +0700

    240441100049_MuhammadAlanAlFarabi_Modul1_AsprakKakYahya

---
+++ modul 1/240441100049_MuhammadAlanAlFarabi/240441100049_MuhammadAlanAlFarabi_Modul1_Soal3_AsprakKakYahya.py
@@ -0,0 +1,11 @@
+# soal
+print("soal no 3")
+print("nilai tukar uang amerika dan indonesia")
+# ketika ingin menukar uang dolar merika ke indonesia harus menggunakan kurs beli
+# kurs beli dolar amerika pada tanggal 25september adalah 15.110,07 di ambil dari https://datacenter.ortax.org/ortax/kursbi/show/USD
+
+dolar_us = 35
+kurs_beli= 15110
+
+tukar   = dolar_us * kurs_beli
+print("hasil uang yang di miliki oleh darmaji adalah ", tukar, "Rp")
