30ec7ca38a9a3e9b4df5a92fc29775628fe0893b
noviamelia <noviamelia572@gmail.com>
2024-10-07 23:28:54 +0700

    240441100033_NoviAmelia_Modul2_Asprakkakyahya

---
+++ modul 2/240441100033_NoviAmelia/tugas1.py/tugas2.py
@@ -0,0 +1,14 @@
+skorJaka = 1100
+ipkJaka = 3.5
+skorIda = 1000
+ipkIda = 3.5
+
+if skorJaka >= 1100 and ipkJaka >=3.0:
+    print("jaka lolos beasiswa berprestasi")
+else:
+    print("jaka tidak lolos beasiswa berprestasi")
+
+if skorIda >= 1100 and ipkJaka >= 3.0 :
+    print("ida lolos beasiswa berprestasi")
+else:
+    print("ida tidak lolos beasiswa berprestasi")
