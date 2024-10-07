311da4ff3aadb0a187b9ea46a857909104540c55
AbyHilmy12 <abyhilmy1212@gmail.com>
2024-10-07 18:25:01 +0700

    Soal_No2.py

--- modul 2/240441100009_AbyHilmy/Soal_No2.py
+++ modul 2/240441100009_AbyHilmy/Soal_No2.py
@@ -2,12 +2,12 @@ skorjaka = 1100
 ipkjaka = 3.5
 skorida = 1000
 ipkida = 3.5
-if skorjaka >=1100 and ipkjaka >= 3:
+if skorjaka >=1100 and ipkjaka >= 3.5:
     print("jaka lulus")
 else:
     print("jaka tidak lulus")
 
-if skorida >=1000 and ipkjaka>= 3:
+if skorida >=1100 and ipkjaka>= 3.5:
     print("Ida lulus")
 else:
     print("Ida tidak lulus")
