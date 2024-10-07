2f085e0656ad9160736a3f0f42ee487b845771bb
MohammadIsmaya <mohismaya29@gmail.com>
2024-10-07 22:58:47 +0700

    240441100093_MohammadIsmaya_Modul2_AsprakKakAngga

--- modul 1/240441100141_M.AndhikaBagusP/tugas3.py
+++
@@ -1,8 +0,0 @@
-# input data
-UangSuraji_Dollar = 35 #Dalam bentuk USD / $
-Kurs = 15.219 / 1 #Kurs 25/9/24 
-
-UangSuraji_Rupiah = UangSuraji_Dollar * Kurs
-
-# Hasil
-print(f"Jumlah uang yang didapatkan oleh Suraji jika dikonversikan ke mata uang rupiah sejumlah Rp.{UangSuraji_Rupiah}")
