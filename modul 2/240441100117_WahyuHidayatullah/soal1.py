704c125b980bbf57116ca7560a8484193e1e1d07
Wahyuu-Hdt <wahyuhidayatullah2024@gmail.com>
2024-10-08 20:51:07 +0700

    240441100117_WahyuHidayatullah

--- modul 2/240441100117_WahyuHidayatullah/soal1.py
+++ modul 2/240441100117_WahyuHidayatullah/soal1.py
@@ -6,16 +6,15 @@ nilai_uas = int(input("MASUKKAN NILAI UAS ANDA: "))
 nilai_rara_rata = (nilai_uts + nilai_uas)/2
 print("NILAI RATA RATA ANDA ADALAH", nilai_rara_rata)
 
-# NILAI RATA RATA
-a = int(input("\nMASUKKAN NILAI RATA RATA ANDA: "))
-
-if a >= 81 <= 90 :
-    print("ANDA MENDAPATKAN NILAI A")
-elif a >= 71 <= 80 :
-    print("ANDA MENDAPATKAN NILAI B")
-elif a >= 61 <= 70 :
-    print("ANDA MENDAPATKAN NILAI C")
-elif a >= 41 <= 60 :
-    print("ANDA MENDAPATKAN NILAI D")
-else : 
-    print("ANDA MENDAPATKAN NILAI E")
+if nilai_rara_rata >= 81 and nilai_rara_rata <= 100 :
+    print("Anda mendapatkan nilai A")
+elif nilai_rara_rata >= 71 and nilai_rara_rata <= 80 :
+    print("Anda mendapatkan nilai B")
+elif nilai_rara_rata >= 61 and nilai_rara_rata <= 70 :
+    print("Anda mendapatkan niali C")
+elif nilai_rara_rata >= 41 and nilai_rara_rata <= 60 :
+    print("Anda mendapatkan nilai D")
+elif nilai_rara_rata >= 0 and nilai_rara_rata <= 40 :
+    print("Anda mendapatkan nilai E")
+else :
+    print("Nilai tidak terdeteksi")
--- modul 2/240441100117_WahyuHidayatullah/soal5.py
+++ modul 2/240441100117_WahyuHidayatullah/soal5.py
@@ -1,27 +1,23 @@
-nama_pembeli = input("MASUKKAN NAMA NAMA ANDA: ")
-usia = int(input("BERAPA USIA ANDA: "))
-member = (input("APAKAH KAMU PUNYA MEMBER: ya/tidak ="))
-total_belanja = int(input("MASUKKAN TOTAL BELANJA ANDA: "))
-diskon_member = 0.15
+nama_pembeli = input("Masukkan nama pembeli : ")
+usia = int(input("Masukkan usia anda : "))
 
-if usia < 18:
-    print("MAAF USIA ANDA BELUM CUKUP")
-if member == "ya":
-    print("SELAMAT ANDA MENDAPATKAN DISKON SEBESAR 15% CUKUP MEMBAYAR", total_belanja - (diskon_member * total_belanja))
-    print("Nama pembeli", nama_pembeli)
-    print("Total Sebelum diskon", total_belanja)
-else:
-    print("Anda harus membayar sebesar", total_belanja)
-    print("Nama pembeli", nama_pembeli)
+if usia < 18 :
+    print("Maaf usia anda belum cukup ")
+    exit()
+else :
+    total_belanja = int(input("Masukkan tottal belanja anda : rp "))
+    member = input("apakah anda memiliki member (y/t)? ")
 
-belanja_500k = 0.10
+diskon = 0
+if member == "y" :
+    diskon += 15
+    if total_belanja > 500000 :
+        diskon += 10
 
-if usia < 18:
-    print("MAAF USIA ANDA TIDAK CUKUP")
-elif total_belanja > 500000:
-    print("SELAMAT ANDA MENDAPATKAN DISKON SEBESAR 10% DAN CUKUP MEMBAYAR", total_belanja - (belanja_500k * total_belanja))
-    print("NAMA PEMBELI:", nama_pembeli)
-    print("TOTAL BELANJA SEBELUM DISKON", total_belanja)
-else:
-    print("TOTAL YANG HARUS DIBAYAR SEBESAR", total_belanja)
-    print("NAMA PEMBELI:", nama_pembeli)
+total_diskon = total_belanja*(diskon/100)
+total_harga_setelah_diskon = (total_belanja - total_diskon)
+
+print("\nNama pembeli : ", nama_pembeli)
+print("Diskon yang di dapat : ", diskon ,"%")
+print("Total harga sebelum diskon : Rp ",total_belanja)
+print("Total harga setelah diskon : Rp ",total_harga_setelah_diskon)
