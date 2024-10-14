323240aefc495dc3e07b948c078091906c39e2f9
ghafirah yutiana <ghafirahyuianah@gmail.com>
2024-10-13 20:37:18 -0700

    240441100101_GhafirahYutiana_Modul3_AsprakKakAngga

---
+++ modul 3/240441100101_GhafirahYutiana/soal1.py
@@ -0,0 +1,16 @@
+tinggi = int(input("Masukkan Tinggi"))
+
+for i in range(tinggi):
+    print("  1")
+print()
+
+for i in range(tinggi):
+    if i == 0 or i == tinggi - 1:  
+        print("00000")  
+    elif i < tinggi - 1:  
+        print("0   0")  
+    print()
+    
+for i in range(tinggi):
+    print("  1")
+print()
