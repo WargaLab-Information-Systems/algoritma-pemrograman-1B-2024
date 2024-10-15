5d7bfe284aedfb180f90256b5ab70289f2272d53
Abdillahfahri <abdillahfahri2705@gmail.com>
2024-10-15 12:51:48 +0700

    240441100021_AbdillahFahriZamani_AsprakKakNuri

---
+++ modul 3/240441100021_AbdillahFahriZamani/soalNo2.py
@@ -0,0 +1,12 @@
+# Inout
+angka = input("Masukkan bilangan bulat: ")
+
+# String untuk menyimpan angka yang dibalik
+balik = " "
+
+# Looping untuk membalik urutan
+for i in angka :
+    balik = i + balik
+
+# Output
+print("Urutan angka setelah dibalik:", balik)
