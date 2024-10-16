61233e8b565ec0d9e6011890e28509c6129b4777
unknown <athinilisti@gmail.com>
2024-10-16 13:42:51 +0700

    240441100129_A'thinilIstiqamahAlmufidah_UTS_AsprakKakIqbal

---
+++ UTS/240441100129_A'thinil-Istiqamah-Almufidah/Praktikum.py
@@ -0,0 +1,12 @@
+y = input ("apakah kamu sudah makan ?:")
+print(y)
+o = input ("apakah kamu sudah mandi ? :")
+print(o)
+transportasi = input ("pilih transportasi")
+waktu = 30
+if waktu==30:
+    print("kamu berangkat tepat waktu")
+else :
+    print("total waktu persiapan : 30 menit")
+
+
