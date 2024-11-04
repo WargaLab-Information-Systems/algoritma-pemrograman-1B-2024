ad4406778e2e3d182be31315ce7a2ef74c254419
MOHSOLEH505 <ayamjagi69@gmail.com>
2024-11-04 16:43:11 +0700

    240441100061_MOH.SOLEH_Modul5_AsprakKakYahya

--- modul 4/240441100061_MOHSOLEH/tugasno1.py
+++ modul 4/240441100061_MOHSOLEH/tugasno1.py
@@ -1,34 +1,19 @@
 n = int(input("Masukkan Ukuran : "))
 karakter = input("Masukkan karakter antara (X / O) : ").upper()
 
-if karakter == "X" or karakter == "O": # or jika nilai salah satunya benar maka benar
-    for i in range(n): # sesuai dengan ukuran yg di masukkan (n)
-        for j in range(n - i - 1): # menentukan jumlah spasi yang dicetak misalnya 7 - 0 - 1=6 di pinggir
-            print(" ", end="") # digunakan untuk mencetak karakter dan spasi tanpa berpindah ke baris baru end
-        for j in range(i + 1): # mencetak huruf yang dimasukkan jadi tambah 1 dan seterusnya
+if karakter == "X" or karakter == "O":
+    for i in range(n): 
+        for j in range(n - i - 1):
+            print(" ", end="")
+        for j in range(i + 1):
             print(karakter + " ", end="")
-        print() #mencetak baris baru
+        print() 
 
     for i in range(n - 1):
-        for j in range(i + 1): # mencetak spasi di pinggir
+        for j in range(i + 1):
             print(" ", end="")
         for j in range(n - i - 1):
             print(karakter + " ", end="")
         print()
 else:
     print("Karakter tidak valid. Silakan masukkan 'X' atau 'O'.")
-
-# elif karakter == "O":
-#     for i in range(n):
-#         for j in range(n - i - 1):
-#             print(" ", end="")
-#         for j in range(i + 1):
-#             print(karakter + " ", end="")
-#         print()
-
-#     for i in range(n - 1):
-#         for j in range(i + 1):
-#             print(" ", end="")
-#         for j in range(n - i - 1):
-#             print(karakter + " ", end="")
-#         print()
