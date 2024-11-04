574fe65405cd297bc95707ce4d74a27909506ad2
oktaviaputrirj <oktaviaputrirj@gmail.com>
2024-11-04 19:04:08 +0700

    240441100057_OktaviaPutriRoichatulJannah__Modul5_AsprakKakYahya

---
+++ modul 5/tugas2.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    if n < 0:
+        print("input anda salah")
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+n = int(input("Fibonacci ke : "))
+print("Hasilnya:", fibonacci(n))
