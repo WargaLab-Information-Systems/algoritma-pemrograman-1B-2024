220abf632cb98428f804b9d7ef701bb2e97e3e18
Athinilistiqamah12 <athinilisti@gmail.com>
2024-11-05 21:43:19 +0700

    240441100129_A'thinilIstiqamahAlmufidah_Modul5_AsprakKakIqbal

---
+++ modul 5/240441100129_A'thinil Istiqamah Almufidah/tugas3.py
@@ -0,0 +1,13 @@
+def faktorial(angka):
+    faktorial = 1
+    for i in range (1, angka + 1):
+        faktorial *= i
+    return faktorial
+
+angka = int(input("Masukkan angka : "))
+
+print(f"{angka}! = ", end="")
+for i in range (angka, 0, -1):
+    print(i,end=" x " if i > 1 else " = ")
+    
+print(faktorial(angka))
