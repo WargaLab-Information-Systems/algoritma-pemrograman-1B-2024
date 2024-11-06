81c6dc7fe35c237ceb4397506a2aa45fdbd062a1
irsyadabdillah18 <muhammadirsyadabdillah18@gmail.com>
2024-11-06 19:52:59 +0700

    240441100017_MohIrsyadAbdillah_Modul5_AsprakKakNuri

--- modul 5/240441100017_Moh.irsyadabdillah/soal no 1.py
+++ modul 5/240441100017_Moh.irsyadabdillah/soal no 1.py
@@ -15,11 +15,11 @@ def calculate_discount(total_belanja, keanggotaan):
     # Menghitung total diskon
     total_diskon = total_belanja * diskon
     harga_setelah_diskon = total_belanja - total_diskon
-    # print(harga_setelah_diskon)
+    
     return harga_setelah_diskon
 
 total_belanja = float(input("Masukkan total belanja: "))
 keanggotaan = input("Masukkan jenis keanggotaan (Gold/Silver/Bronze/Tidak Ada): ")
 calculate_discount(total_belanja,keanggotaan)
-# harga_final = calculate_discount(total_belanja, keanggotaan)
-# print("Total yang harus dibayar setelah diskon:", harga_final)
+harga_final = calculate_discount(total_belanja, keanggotaan)
+print("Total yang harus dibayar setelah diskon:", harga_final)
